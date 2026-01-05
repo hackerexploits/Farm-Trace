a01bc446eddd8ceb958de6cddd46989523602e85
maniamartial <martialmania19@gmail.com>
2026-01-06 00:38:37 +0300

    feat: add fields

---
+++ farmtrace/farm_trace/doctype/district/district.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2026, Mania and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("District", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
---
+++ farmtrace/farm_trace/doctype/farm/farm.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2026, Mania and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Farm", {
+	refresh(frm) {
+		// Auto-fetch district, state, and country when village is selected
+		if (frm.doc.village) {
+			frm.trigger("village");
+		}
+	},
+	
+	village(frm) {
+		if (frm.doc.village) {
+			frappe.db.get_doc("Village", frm.doc.village)
+				.then(doc => {
+					if (doc.district) {
+						frm.set_value("district", doc.district);
+					}
+					if (doc.state) {
+						frm.set_value("state", doc.state);
+					}
+					if (doc.country) {
+						frm.set_value("country", doc.country);
+					}
+				});
+		} else {
+			frm.set_value("district", "");
+			frm.set_value("state", "");
+			frm.set_value("country", "");
+		}
+	}
+});
+
